6b0a9a0aa15c57f624ce4dce0036b64a3015e350
Scott Wisniewski <scott@scottdw2.com>
2020-02-17 22:30:21 -0800

    Add installer / code signing.

---
+++ installer/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+BINARY_DIR="$1"
+SIGN_IDENTITY="$2"
+
+DIRNAME="$(dirname $0)"
+TARGET_DIR="ROOT/tmp/QhyCfw3Usb_X2"
+REVISION="$(git rev-list HEAD --count)"
+
+mkdir -p ${TARGET_DIR}
+cp "${DIRNAME}/../filterwheellist QHYCFW3.txt" "${TARGET_DIR}"
+cp "${BINARY_DIR}/libqhycfw3_usb.dylib" "${TARGET_DIR}"
+
+pkgbuild \
+  --root ROOT \
+  --identifier com.scottdw2.QhyCfw3Usb_X2 \
+  --sign "${SIGN_IDENTITY}" \
+  --version 0.1.${REVISION} \
+  --scripts ${DIRNAME}/scripts \
+  ${BINARY_DIR}/QhyCfw3Usb_x2.pkg
+
+pkgutil --check-signature ${BINARY_DIR}/QhyCfw3Usb_x2.pkg
+rm -rf ROOT
